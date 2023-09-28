ccae1ff58a1faeec5b3a38c29aa8abdae28c2617
JanNorell <101710309+JanNorell@users.noreply.github.com>
2023-09-28 10:48:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-print('Omega 3 & Blueberry Oatmeal')
-print('Kale Spinach & Rocket Smoothie')
-Print('Hard-Boild Free-Range Egg')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smootie')
+streamlit.text('Hard-Boild Free-Range Egg')
