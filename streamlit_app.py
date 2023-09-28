70404d9c31f6951049fc3d8ea9e4322c4a956c70
JanNorell <101710309+JanNorell@users.noreply.github.com>
2023-09-28 10:45:14 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+print('Omega 3 & Blueberry Oatmeal')
+print('Kale Spinach & Rocket Smoothie')
+Print('Hard-Boild Free-Range Egg')
